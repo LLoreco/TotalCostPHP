4a83fdbb4af3f34de8e7cafe48c12bb0a13e8e14
Kirill Safin <safinkirill2003@mail.ru>
2025-06-14 15:45:34 +0500

    Created calculator

---
+++ services/Calculator.php
@@ -0,0 +1,19 @@
+<?php
+    class Calculator{
+        private $allDiscounts;
+
+        public function __construct(array $discounts)
+        {
+            $this->allDiscounts = $discounts;
+        }
+
+        public function calculateTotalPrice(array $items, float $total) : float{
+            $totalPrice = $total;
+            foreach ($this->allDiscounts as $discounts){
+                $discount = $discounts->apply($totalPrice);
+                $totalPrice -= $discount;
+            }
+            return round($totalPrice,2);
+        }
+    }
+?>
